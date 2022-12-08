a1faaa80401da007c48d0f1d53294b259ae74c93
François Mazeau <francois.mazeau@datadoghq.com>
2022-12-08 14:13:58 +0100

    env.sh: remove staging specific overrides

--- env.sh
+++ env.sh
@@ -10,7 +10,4 @@ export DD_DOGSTATSD_NON_LOCAL_TRAFFIC="true"
 export DD_REMOTE_CONFIGURATION_ENABLED="true"
 export DD_API_KEY="$(vault kv get -format json applications/datadog-agent/shared/agent_api_key_appsec_test_org | jq -r .data.value)"
 export DD_REMOTE_CONFIGURATION_KEY="$(vault kv get -format json applications/datadog-agent/shared/agent_remote_config_key_appsec_test_org | jq -r .data.value)"
-export DD_REMOTE_CONFIGURATION_CONFIG_ROOT="$(vault kv get -format json applications/datadog-agent/shared/agent_remote_config_config_root | jq -r .data.value)"
-export DD_REMOTE_CONFIGURATION_DIRECTOR_ROOT="$(vault kv get -format json applications/datadog-agent/shared/agent_remote_config_director_root | jq -r .data.value)"
-export DD_REMOTE_CONFIGURATION_REFRESH_INTERVAL=5s
 unset VAULT_ADDR
